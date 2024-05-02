422d6a4eb3b0fd83b2348de5fbc4b76f1b3a5a20
dulalchakrabarti <dulalchakrabarti@gmail.com>
2024-05-02 17:48:06 +0530

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -223,7 +223,7 @@ class Dataset_Custom(Dataset):
         df_raw.columns: ['date', ...(other features), target feature]
         '''
         cols = list(df_raw.columns);
-        print(cols)
+        #print(cols)
         if self.cols:
             cols=self.cols.copy()
             cols.remove(self.target)
